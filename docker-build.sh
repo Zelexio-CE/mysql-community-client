80840ae24058e7148e5f52dd26587e23254ad661
Simon Descarries <40470761+sdescarries@users.noreply.github.com>
2026-02-01 12:41:27 -0500

    Chore: Upgrade to latest version 8.4.8 (#2)

--- docker-build.sh
+++ docker-build.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-VERSION=8.4.7
+VERSION=8.4.8
 
 docker buildx build \
   --build-arg VERSION=${VERSION} \
   --file "Dockerfile" \
   --platform linux/amd64,linux/arm64/v8 \
   --progress plain \
-  --provenance=true \
+  --provenance=mode=max \
   --pull \
   --sbom=true \
-  --tag "zelexio/mysql-community-client:${VERSION}" \
-  --tag "zelexio/mysql-community-client:latest" \
+  --tag "docker.io/zelexio/mysql-community-client:${VERSION}" \
+  --tag "docker.io/zelexio/mysql-community-client:latest" \
+  --tag "ghcr.io/zelexio-ce/mysql-community-client:${VERSION}" \
+  --tag "ghcr.io/zelexio-ce/mysql-community-client:latest" \
   "${@}" \
   "."
+
+docker scout cves \
+  --locations \
+  --format markdown \
+  --output "vuln/vuln-mysql-community-client:${VERSION}.md" \
+  "${tag}"
